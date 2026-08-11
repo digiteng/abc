3dcaf385abf1844862f6cc89886a2fb4997ba240
digiteng <digiteng@gmail.com>
2026-08-11 03:19:44 +0300

    Modify home route to return static response
    Updated home route to return a simple string instead of fetching from GitHub.

--- main.py
+++ main.py
@@ -3,19 +3,12 @@ import requests
 import os # İşletim sistemi modülünü ekleyin
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 #GITHUB_URL = os.environ.get('GITHUB_URL')
-GITHUB_URL = 'https://api.github.com/repos/digiteng/xe8/contents/xtra.py'
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-
-
-    header = {
-        'accept': 'application/vnd.github.v3.raw', 
-        'authorization': f'token {GITHUB_TOKEN}'
-    }
-    
-    req = requests.get(GITHUB_URL, stream=True, allow_redirects=True, headers=header).text
+    req="working..."
     return req
 
 @app.route('/about')
