5211182531bc0d13994c9ffd35f402147d4f4d2b
digiteng <digiteng@gmail.com>
2026-08-11 03:00:45 +0300

    Modify home route to return GitHub API response
    Updated home route to fetch content from GitHub API.

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return {"status": "Python projesi Vercel üzerinde çalışıyor!"}
+	#return {"status": "Python projesi Vercel üzerinde çalışıyor!"}
+	url = 'https://api.github.com/repos/digiteng/xe8/contents/xtra.py'
+	header={'accept': 'application/vnd.github.v3.raw', 'authorization': 'token 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'}
+	req = requests.get(url, stream=True, allow_redirects=True, headers=header).text
+	return req
 
 @app.route('/about')
 def about():
