0a7a3a543d028fd33cb9511a5302ca142b7a32b3
digiteng <digiteng@gmail.com>
2026-08-11 03:25:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from flask import Flask
 import requests
 import os # İşletim sistemi modülünü ekleyin
 GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
-#GITHUB_URL = os.environ.get('GITHUB_URL')
+GITHUB_URL = os.environ.get('GITHUB_URL')
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     req="working..."
-    return GITHUB_TOKEN
+    header={'accept': 'application/vnd.github.v3.raw', 'authorization': f'token {GITHUB_TOKEN}'}
+    req = requests.get(GITHUB_URL, headers=header)
+    return req
 
 @app.route('/about')
 def about():
