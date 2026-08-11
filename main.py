9c3a1ad811f00fcdbc786c67092286a9da854812
digiteng <digiteng@gmail.com>
2026-08-11 03:07:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from flask import Flask
 import requests
+import os # İşletim sistemi modülünü ekleyin
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+GITHUB_URL = os.environ.get('GITHUB_URL')
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	#return {"status": "Python projesi Vercel üzerinde çalışıyor!"}
-	url = 'https://api.github.com/repos/digiteng/xe8/contents/xtra.py'
-	header={'accept': 'application/vnd.github.v3.raw', 'authorization': 'token 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'}
-	req = requests.get(url, stream=True, allow_redirects=True, headers=header).text
-	return req
+
+
+    header = {
+        'accept': 'application/vnd.github.v3.raw', 
+        'authorization': f'token {GITHUB_TOKEN}'
+    }
+    
+    req = requests.get(URL, stream=True, allow_redirects=True, headers=header).text
+    return req
 
 @app.route('/about')
 def about():
-	return "Hakkımızda sayfası"
+    return "Hakkımızda sayfası"
 
-# Vercel için bu kısım önemlidir
 if __name__ == '__main__':
-	app.run()
+    app.run()
+
